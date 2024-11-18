4881d9c210841ca9a5ed197b6cbd579ba5c43707
Sebastian Vielma J. <sebastian.vielma03@inacapmail.cl>
2024-11-18 04:11:24 -0300

    falta css

--- evaluaciones/sumativa3/sumativa2/settings.py
+++ evaluaciones/sumativa3/sumativa2/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^cnvz&y@57$-3zq3*$3gw$&#%$6ayfh^y(_cyq17f65f$mjq2f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
